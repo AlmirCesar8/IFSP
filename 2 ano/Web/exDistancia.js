3c0bb704fea5d257b739940ffc48520496fe8cb5
Almir Cesar <cesar25silvafilho@gmail.com>
2025-05-08 08:36:58 -0300

    exWEB

---
+++ 2 ano/Web/exDistancia.js
@@ -0,0 +1,6 @@
+let velocidadeMedia = 80, tempo = 4;
+let distancia = velocidadeMedia * tempo;
+let litrosUsados = Math.floor(distancia/12)
+
+console.log("A distância pecorrida foi de " +distancia+ "km, a velocidade media foi de "+ velocidadeMedia+" km/h e tempo gasto foi de "+tempo+" horas");
+console.log("Foram usados " + litrosUsados+"L, aproximadadente");
---
+++ 2 ano/Web/exPropriedadeDistribuitiva.js
@@ -0,0 +1,14 @@
+const A = 9, B = 3.7, C = 2, D = 8.5;
+
+console.log("A + B = " + (A + B));
+console.log("A * B = " + (A * B));
+console.log("A + C = " + (A + C));
+console.log("A * C = " + (A * C));
+console.log("A + D = " + (A + D));
+console.log("A * D = " + (A * D));
+console.log("B + C = " + (B + C));
+console.log("B * C = " + (B * C));
+console.log("B + D = " + (B + D));
+console.log("B * D = " + (B * D));
+console.log("C + D = " + (C + D));
+console.log("C * D = " + (C * D));
